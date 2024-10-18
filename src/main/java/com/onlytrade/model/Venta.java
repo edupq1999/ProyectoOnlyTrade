e3c23b0e3b1d88d291d99670005626cedb315b85
edupq1999 <nivel.17@hotmail.com>
2024-10-17 22:16:20 -0500

    Se hizo las relaciones de la venta

--- src/main/java/com/onlytrade/model/Venta.java
+++ src/main/java/com/onlytrade/model/Venta.java
@@ -1,29 +1,54 @@
 package com.onlytrade.model;
 
 import java.util.Date;
-
+import java.util.List;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@Table(name = "tb_venta")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Venta {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer ventaId;
 
-	@Column(name = "usuario", nullable = false)
-	private String usuario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ventaId;
 
-	@Column(name = "producto", nullable = false)
-	private Integer producto;
+    @Column(name = "cantidad", nullable = false)
+    private Integer cantidad;
 
-	@Column(name = "cantidad", nullable = false)
-	private Double cantidad;
+    @Column(name = "montoTotal", nullable = false)
+    private Double montoTotal;
 
-	@Column(name = "montoTotal", nullable = false)
-	private String montoTotal;
+    @Column(name = "fVenta", nullable = false)
+    private Date fVenta;
 
-	@Column(name = "fVenta", nullable = false)
-	private Date fVenta;
+    @ManyToOne
+    @JoinColumn(name = "fk_usuario", referencedColumnName = "usuarioId", nullable = false)
+    private Persona persona;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tb_venta_producto",
+        joinColumns = @JoinColumn(name = "venta_id"),
+        inverseJoinColumns = @JoinColumn(name = "producto_id")
+    )
+    private List<Producto> productos;
 }
