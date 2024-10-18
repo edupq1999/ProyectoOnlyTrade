278653297ff9505d1ecef7da2d2942ed7b23a6d0
edupq1999 <nivel.17@hotmail.com>
2024-10-17 22:22:24 -0500

    Se arreglaron los repositorios

--- src/main/java/com/onlytrade/repository/PersonaRepository.java
+++ src/main/java/com/onlytrade/repository/PersonaRepository.java
@@ -5,10 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.onlytrade.model.Persona;
-import com.onlytrade.model.Producto;
 
 
 public interface PersonaRepository extends JpaRepository<Persona, Integer>{
-	List<Persona>listarPersona();
 	List<Persona>findByBuscarNombrePersona(String NombrePersona);
 }
--- src/main/java/com/onlytrade/repository/ProductoRepository.java
+++ src/main/java/com/onlytrade/repository/ProductoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.onlytrade.model.Producto;
 
+
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
-	List<Producto>listarProducto();
 	List<Producto>findByBuscarNombreProducto(String NombreProducto);
 }
