ef40346763993476a2f6e5ce692e3bf881a47440
edupq1999 <nivel.17@hotmail.com>
2024-10-19 22:18:57 -0500

    Se cambio el tipo de dato del parametro

--- src/main/java/com/onlytrade/service/CuentaService.java
+++ src/main/java/com/onlytrade/service/CuentaService.java
@@ -8,5 +8,5 @@ public interface CuentaService {
 	List<Cuenta>listarCuenta();
 	void crearCuenta(Cuenta newCuenta);
 	void actualizarCuenta(Cuenta updatedCuenta);
-	void eliminarCuenta(Integer cuentaId);
+	void eliminarCuenta(String cuentaId);
 }
