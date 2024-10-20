9b64bea871573be4c128ba92440936169745fd50
edupq1999 <nivel.17@hotmail.com>
2024-10-19 22:18:17 -0500

    Se elimino metodo no utilizado

--- src/main/java/com/onlytrade/repository/PersonaRepository.java
+++ src/main/java/com/onlytrade/repository/PersonaRepository.java
@@ -1,12 +1,10 @@
 package com.onlytrade.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.onlytrade.model.Persona;
 
 
 public interface PersonaRepository extends JpaRepository<Persona, Integer>{
-	List<Persona>findByBuscarNombrePersona(String NombrePersona);
+
 }
