c80b0fbc84ed0eea7ac6038fe5bf7ec200b27228
JorgeGallegos03 <gallegosjorge544@gmail.com>
2024-10-26 04:41:34 -0500

    Se acomo el repositorio

--- src/main/java/com/onlytrade/repository/ProductoRepository.java
+++ src/main/java/com/onlytrade/repository/ProductoRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.onlytrade.model.Categoria;
 import com.onlytrade.model.Producto;
 
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 	List<Producto>findByNombre(String NombreProducto);
-	List<Producto>findByCategoriaId(Integer idCategoria);
+	List<Producto>findByCategoria(Categoria categoria);
 }
